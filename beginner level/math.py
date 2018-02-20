6f1482adca25e49c10495389400375e7e0b2e7c1
tanyabalamurali <36623831+tanyabalamurali@users.noreply.github.com>
2018-02-20 10:20:52 +0530

    Update and rename positive.py to math.py

---
+++ beginner level/math.py
@@ -0,0 +1,16 @@
+num1 = 1.5
+num2 = 6.3
+num3 = 0.0
+num4 = 6.3
+num5 = 1.5
+num6 = 0.0
+num7 = 20
+num8 = 2
+num9 = 0
+num10 = 6.3
+num3 = float(num1) + float(num2)
+num6 = float(num4) * float(num5)
+num9 = int(num7) / int(num8)
+print('The sum of {0} and {1} is {2}'.format(num1, num2, num3))
+print('The mul of {0} and {1} is {2}'.format(num4, num5, num6))
+print('The div of {0} and {1} is {2}'.format(num7, num8, num9))
