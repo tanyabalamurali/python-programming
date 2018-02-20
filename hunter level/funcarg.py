82804157149993ee0eb56e89cd22baaaf624232f
tanyabalamurali <36623831+tanyabalamurali@users.noreply.github.com>
2018-02-20 14:17:51 +0530

    Create funcarg.py

---
+++ hunter level/funcarg.py
@@ -0,0 +1,15 @@
+
+def Exploder(string,n):
+    print(string*n)
+    def MemoryErroryfun(string,exploder,n):
+
+        for i in range(5):
+
+         print(string)
+
+
+
+
+
+if __name__ == '__main__':
+    Exploder('guvi',5)
